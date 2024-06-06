a82e95bb53da53c3ea10fbb0798030153219f33a
Muhoza John <muhozajohn250@gmail.com>
2024-06-06 18:49:29 +0200

    Adding Image Link

--- src/services/users.services.js
+++ src/services/users.services.js
@@ -26,7 +26,7 @@ export const createUser = async (userData, file) => {
     name: userData.name,
     email: userData.email,
     password: hashedPass,
-    img: result?.secure_url,
+    img: result?.secure_url || "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
   });
 };
 
